9c7b275861f434004c18c1874697567f367afbbd
Nicolás Sebastián Estigarribia <nicolas.sebastian.estigarribia@gmail.com>
2023-05-16 12:17:39 -0300

    Docker container y imagen. Flutter V3.7.12

--- workspace/Flutter-Movies/lib/utils/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const darkBlue = Color(0x44007acc);
-const lightBlue = Color.fromRGBO(0, 149, 246, 1);
-const primaryColor = Colors.white;
-const secondaryColor = Colors.grey;
